b3fb9aea3eb8bbcbad42a11ca42b5bfa190a268a
aslamhus <aslam.percy.husain@gmail.com>
2025-04-18 23:50:41 -0700

    add minimization to production build

--- src/files/webpack.prod.js
+++ src/files/webpack.prod.js
@@ -3,4 +3,7 @@ const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+  },
 });
